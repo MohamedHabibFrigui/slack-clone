05b0ceda26543b49e020cc3534ca996cd475c53e
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-08-27 10:52:26 +0100

    Add auth ui (sign in and sign up)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
+import AuthScreen from "@/features/auth/components/AuthScreen";
 
 export default function Home() {
-  return (
-    <div className="text-rose-500 font-bold text-2xl">
-      <Button>Click me</Button>
-    </div>
-  );
+  return <AuthScreen />;
 }
