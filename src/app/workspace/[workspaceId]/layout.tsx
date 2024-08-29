e94deca5c130b7e546e534f2c2af32deabe7f6e4
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-08-29 14:20:41 +0100

    Add workspace sidebar structure and current member

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,12 @@
 
 import Sidebar from "./Sidebar";
 import Toolbar from "./Toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import WorkspaceSidebar from "./WorkspaceSidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -13,7 +19,21 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="slack-workspace-layout"
+          autoSave="true"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
