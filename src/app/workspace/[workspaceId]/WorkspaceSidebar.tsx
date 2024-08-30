cbace326fc4f98d6a3fb9c715ad322284f6b4f37
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2024-08-30 17:18:40 +0100

    Add create a new channel endpoint and modal

--- src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
+++ src/app/workspace/[workspaceId]/WorkspaceSidebar.tsx
@@ -14,9 +14,12 @@ import { useGetChannels } from "@/features/channels/api/useGetChannels";
 import WorkspaceSection from "./WorkspaceSection";
 import useGetMembers from "@/features/members/api/useGetMembers";
 import UserItem from "./UserItem";
+import { useCreateChannelModal } from "@/features/channels/store/useCreateChannelModal";
 
 const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
+  const [_open, setOpen] = useCreateChannelModal();
+
   const { data: member, isLoading: memberLoading } = useCurrentMember({
     workspaceId,
   });
@@ -58,7 +61,11 @@ const WorkspaceSidebar = () => {
         <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="drafts" />
       </div>
 
-      <WorkspaceSection label="Channels" hint="New channel" onNew={() => {}}>
+      <WorkspaceSection
+        label="Channels"
+        hint="New channel"
+        onNew={member.role === "admin" ? () => setOpen(true) : undefined}
+      >
         {channels?.map((item) => (
           <SidebarItem
             key={item._id}
--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CreateChannelModal from "@/features/channels/components/CreateChannelModal";
 import CreateWorkspaceModal from "@/features/workspaces/components/CreateWorkspaceModal";
 import { useEffect, useState } from "react";
 
@@ -14,6 +15,7 @@ export const Modals = () => {
 
   return (
     <>
+      <CreateChannelModal />
       <CreateWorkspaceModal />
     </>
   );
